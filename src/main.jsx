0d800231eb4c191d51e9c6af25143ddb6d600fa2
edgeofunknowns <ramachandra.rakshith@gmail.com>
2025-09-12 10:24:15 -0400

    Add error boundary to prevent blank screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-createRoot(document.getElementById('root')).render(<App />)
+import ErrorBoundary from './ErrorBoundary.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+)
